80ce01e83859c01249f60869eb8950e79b51541e
spendlively <spendlively@mail.ru>
2017-10-10 16:26:23 +0700

    Подправил неймспейс

--- php/persistent_sessions/php_sessions/advanced_session_handler/test/includes/init.php
+++ php/persistent_sessions/php_sessions/advanced_session_handler/test/includes/init.php
@@ -1,16 +1,12 @@
 <?php
 use Foundationphp\Sessions\PersistentSessionHandler;
 
-require_once __DIR__ . '/../../classes/PersistentProperties.php';
-require_once __DIR__ . '/../../classes/AutoLogin.php';
-require_once __DIR__ . '/../../classes/MysqlSessionHandler.php';
-require_once __DIR__ . '/../../classes/PersistentSessionHandler.php';
 require_once __DIR__ . '/Psr4AutoloaderClass.php';
 require_once __DIR__ . '/db_connect.php';
 
 $loader = new Psr4AutoloaderClass();
 $loader->register();
-$loader->addNamespace('Foundationphp', __DIR__ . '/../../Foundationphp');
+$loader->addNamespace('Foundationphp\\Sessions\\', __DIR__ . '/../../classes/');
 
 $handler = new PersistentSessionHandler($db);
 session_set_save_handler($handler);
